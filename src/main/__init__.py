9666ee57c8b01486d67be108adae9ee742111b2f
Svalgo <svalgo@itcollege.ee>
2017-03-15 16:48:36 +0200

    add .gitignore

---
+++ src/main/__init__.py
@@ -0,0 +1,8 @@
+
+"""An application for viewing and manipulating .csv files
+    BasicGui is to open and view files in a  window.
+    CSV_reader Has the functionality to manipulate comma separated value files.
+    Since there are only guidelines on how to create .csv files, there are many different ways the files
+    behave. This Application can only handle files with a comma "," as a delimiter.
+    """
+
